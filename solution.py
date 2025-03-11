4c3bdf95f58d6122b5e61974dd9db08b95b4afbe
milanastremlina <m.stremlina@bk.ru>
2025-03-12 00:20:08 +0300

    add solution

---
+++ solution.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    return """Человечество вырастает из детства.<br>
+Человечеству мала одна планета.<br>
+Мы сделаем обитаемыми безжизненные пока планеты.<br>
+И начнем с Марса!<br>
+Присоединяйся!<br>"""
+
+
+@app.route('/image_mars')
+def image_mars():
+    return """<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="/static/img/mars.jpg" alt="здесь должна была быть картинка, но не нашлась"
+                    height=500 width=500>
+                    <br>Вот она какая, красная планета.
+                  </body>
+                </html>"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
