075dd471d53a5ec4a43198bb7c5c69a61a037e04
milanastremlina <m.stremlina@bk.ru>
2025-03-15 18:47:44 +0300

    add solution

---
+++ solution.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    engineering = False
+    if 'инженер' in prof or 'строитель' in prof:
+        engineering = True
+    return render_template('training.html', engineering=engineering)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
