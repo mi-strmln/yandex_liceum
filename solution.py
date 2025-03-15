dced1af37ad418cbc506cb043efece7f3d5ed843
milanastremlina <m.stremlina@bk.ru>
2025-03-15 22:06:40 +0300

    add solution

---
+++ solution.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return render_template('success.html', title='Успешная авторизация', data=form.id_astr)
+    return render_template('login.html', title='Аварийный доступ', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
