6110249a3c7e9f8762170b67b30712e23362d857
milanastremlina <m.stremlina@bk.ru>
2025-03-19 19:02:38 +0300

    add loginform.py

---
+++ loginform.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+# from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    id_astr = StringField('id астронавта', validators=[DataRequired()])
+    password_astr = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    id_cap = StringField('id капитана', validators=[DataRequired()])
+    password_cap = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
