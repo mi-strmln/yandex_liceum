74988218db2ccd609bc3eb5475fbe4efe702c1be
milanastremlina <m.stremlina@bk.ru>
2025-03-20 21:30:20 +0300

    add "Тестирование удаления" solution

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from requests import get, post, delete
 
 
-print(delete('http://localhost:5000/api/jobs/3').json())
+print(delete('http://localhost:5000/api/jobs/4').json()) # корректный запрос
+print(delete('http://localhost:5000/api/jobs/150').json()) # некорректный (с несуществующим id) запрос
+print(delete('http://localhost:5000/api/jobs/').json()) # некорректный (пустой) запрос
+print(delete('http://localhost:5000/api/jobs/с').json()) # некорректный (с неверным форматом id) запрос
+
 print(get('http://localhost:5000/api/jobs').json())
 
