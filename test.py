9e0673d9ffb00cba47cb6328b01b5da9fc0cfdf3
milanastremlina <m.stremlina@bk.ru>
2025-03-20 20:36:18 +0300

    add "Тестирование POST-запроса" solution

--- test.py
+++ test.py
@@ -1,12 +1,25 @@
 from requests import get, post
 
-#print(get('http://localhost:5000/api/jobs').json())
-#print(get('http://localhost:5000/api/jobs/1').json())
-#print(get('http://localhost:5000/api/jobs/100').json())
-#print(get('http://localhost:5000/api/jobs/f').json())
+
 print(post('http://localhost:5000/api/jobs',
            json={'team_leader': 1,
-                 'job': 'equipment check',
-                 'work_size': 24,
-                 'collaborators': '2, 3',
-                 'is_finished': False}).json())
+                 'job': 'air analysis',
+                 'work_size': 10,
+                 'collaborators': '2',
+                 'is_finished': False}).json()) # корректный запрос
+print()
+print(post('http://localhost:5000/api/news', json={}).json()) # некорректный (пустой) запрос
+print()
+print(post('http://localhost:5000/api/news',
+           json={'team_leader': 2}).json()) # некорректный (с не полным набором полей) запрос
+print()
+print(post('http://localhost:5000/api/jobs',
+           json={'team_lider': 1,
+                 'job': 'air analysis',
+                 'work_size': 10,
+                 'collaborators': '2',
+                 'is_finished': False}).json()) # некорректный (с неправильным назанием поля) запрос
+
+print()
+print(get('http://localhost:5000/api/jobs').json())
+
