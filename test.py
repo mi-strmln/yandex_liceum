2d44be4e9770ecdd2c6e4b89ed10ab2523719b50
milanastremlina <m.stremlina@bk.ru>
2025-03-20 21:49:05 +0300

    add "Тестирование редактирования " solution

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from requests import get, post, delete, put
 
+print(put('http://localhost:5000/api/jobs/1',
+          json={'job': 'deployment of residential module 2'}).json())  # корректный запрос
 
-print(put('http://localhost:5000/api/jobs/1', json={
-            'job': 'deployment of residential module 2'
-        }).json())
+print(put('http://localhost:5000/api/jobs/1',
+          json={}).json())  # некорректный (пустой) запрос
+print(put('http://localhost:5000/api/jobs/150',
+          json={'job': 'deployment of residential module 2'}).json())  # некорректный (с несуществующим id) запрос
+print(put('http://localhost:5000/api/jobs/с',
+          json={'job': 'deployment of residential module 2'}).json())  # некорректный (с неверным форматом id) запрос
+print(put('http://localhost:5000/api/jobs/150',
+          json={'leader_id': 1}).json())  # некорректный (с неверным именем поля) запрос
 
 print(get('http://localhost:5000/api/jobs').json())
-
