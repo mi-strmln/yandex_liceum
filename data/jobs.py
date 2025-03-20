9227bfb9245c51b98cfa808a4a433be2fc13a691
milanastremlina <m.stremlina@bk.ru>
2025-03-20 17:04:13 +0300

    add "Получение всех работ" solution

--- data/jobs.py
+++ data/jobs.py
@@ -1,9 +1,12 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import orm
 
 
-class Jobs(SqlAlchemyBase):
+
+class Jobs(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'jobs'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
