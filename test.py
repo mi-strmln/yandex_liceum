00f3623cf24271155d35f4f8a1850b71fb4938c4
milanastremlina <m.stremlina@bk.ru>
2025-03-20 18:02:14 +0300

    add "Тестирование ответа сервера" solution

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from requests import get
 
-#print(get('http://localhost:5000/api/jobs').json())
+print(get('http://localhost:5000/api/jobs').json())
 print(get('http://localhost:5000/api/jobs/1').json())
+print(get('http://localhost:5000/api/jobs/100').json())
+print(get('http://localhost:5000/api/jobs/f').json())
