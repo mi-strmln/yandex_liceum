9372e4ae5e081fbb9bcecb84476d7f98c468dbc1
milanastremlina <m.stremlina@bk.ru>
2025-05-15 21:44:03 +0300

    add solution

---
+++ solution.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route("/distribution")
+def distribution():
+    members = [
+        "Ридли Скотт",
+        "Энди Уир",
+        "Марк Уотни",
+        "Венката Капур",
+        "Тедди Сандерс",
+        "Шон Бин"
+    ]
+    return render_template("distribution.html", members=members)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
