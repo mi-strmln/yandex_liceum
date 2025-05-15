3b813ecdbe4f9b1d9de02d4ba8a9b9749cd5b8b6
milanastremlina <m.stremlina@bk.ru>
2025-05-15 15:31:42 +0300

    add "Форма регистрации" solution

---
+++ data/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    """форма регистрации"""
+    login = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm = PasswordField('Repeat password', validators=[DataRequired()])
+    surname = StringField('Surname')
+    name = StringField('Name')
+    age = StringField('Age')
+    position = StringField('Position')
+    speciality = StringField('Speciality')
+    address = StringField('Address')
+    submit = SubmitField('Sign up')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
+from data.forms import RegisterForm
 import datetime
 
 # from forms.user import RegisterForm
@@ -9,11 +10,36 @@ import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+
 @app.route("/")
 def journal_works():
     session = db_session.create_session()
     jobs = session.query(Jobs).all()
     return render_template("journal_works.html", title='List of Jobs', jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.confirm.data:
+            return render_template('register.html', title='Registration',
+                                   form=form,
+                                   message="Passwords don't match")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.login.data).first():
+            return render_template('register.html', title='Registration',
+                                   form=form,
+                                   message="This user already exists")
+        user = User(
+            name=form.name.data,
+            email=form.login.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Registration', form=form)
 
 
 def main():
