13fe6539ae67c741436f485494e1d4842d629ba7
milanastremlina <m.stremlina@bk.ru>
2025-05-15 15:53:35 +0300

    add "Отображение формы авторизации" solution

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
-from data.forms import RegisterForm
+from data.forms import RegisterForm, LoginForm
+from flask_login import login_user, logout_user, login_required, current_user, LoginManager
 import datetime
 
 # from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(id)
 
 
 @app.route("/")
@@ -40,6 +50,28 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Registration', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Wrong email or password",
+                               form=form)
+    return render_template('login.html', title='Authorisation', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 def main():
